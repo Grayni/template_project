2d5b15f3b8d6355827dfeb41273e23e71e6ed5e2
grayni <Omenbestg@gmail.com>
2017-10-29 18:12:02 +0300

    new(project) add default file

---
+++ name_project/app/js/libs.js
@@ -0,0 +1,8 @@
+//= ../libs/jquery/dist/jquery.min.js
+//= ../libs/popper.js/dist/umd/popper.min.js
+//= ../libs/bootstrap/js/dist/tooltip.js
+//= ../libs/bootstrap/js/dist/button.js
+//= ../libs/bootstrap/js/dist/util.js
+//= ../libs/bootstrap/js/dist/popover.js
+//= ../../node_modules/clipboard/dist/clipboard.min.js
+//= custom.js
